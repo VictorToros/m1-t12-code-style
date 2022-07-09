8eddd07ead3a77acb8a1b657d59742cee9da95a4
Stanislav <stanislavshchekochikhin@yandex.ru>
2022-07-09 11:50:16 +0300

    добавлены комментарии по codeStyle

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
-public class DepositCalculator  {
+public class DepositCalculator  { // перед фигурной скобкой стоят два пробела вместо одного
     double calculateComplexPercentFunction(double doubleAmount, double doubleYearRate, int depositPeriod) {
+        // постфикс Function в названии по условию не нужен и в названиях переменных указывать их тип (double) не надо
         double pay = doubleAmount * Math.pow ((1 + doubleYearRate / 12), 12 * depositPeriod);
         return round(pay, 2);
     }
 
     double calculateSimplePercentFunction(double doubleAmount, double doubleYearRate, int depositPeriod) {
+        // постфикс Function в названии по условию не нужен и в названиях переменных указывать их тип (double) не надо
         return round (doubleAmount + doubleAmount * doubleYearRate * depositPeriod, 2);
     }
 
@@ -15,7 +17,7 @@ public class DepositCalculator  {
         return Math.round (value * scale) / scale;
     }
 
-    void doImportantJob() {
+    void doImportantJob() { // по условию требуется какое-то название, описывающее суть происходящего внутри
         int period;
         int action;
         final double yearRate = 0.06;
@@ -27,8 +29,10 @@ public class DepositCalculator  {
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double outDoubleVar = 0;
+        double outDoubleVar = 0; // в названиях переменных тип самой переменной (Double) и того,
+        // что это переменная (Var) быть не должно
         if (action == 1) outDoubleVar = calculateSimplePercentFunction(amount, yearRate, period);
+        // аналогично предыдущему
         else if (action == 2) {
             outDoubleVar = calculateComplexPercentFunction(amount, yearRate, period);
         }
@@ -38,5 +42,5 @@ public class DepositCalculator  {
     public static void main(String[] args) {
         new DepositCalculator().doImportantJob();
     }
-
+// мне кажется, эта пропущенная строка - лишняя
 }
